d37154a371fd8f7764ed2f1885b3c1d3a7792934
khosta77 <puwerfulpants@gmail.com>
2025-11-22 12:40:08 +0300

    Оптимизиция запросов

--- project/app/repositories/question_repository.py
+++ project/app/repositories/question_repository.py
@@ -4,7 +4,7 @@
 
 from __future__ import annotations
 
-from django.db.models import QuerySet
+from django.db.models import Count, QuerySet
 
 from app.interfaces import IQuestionRepository
 from app.models import Question
@@ -19,6 +19,7 @@ class QuestionRepository(IQuestionRepository):
             Question.objects.new()
             .select_related("author", "author__profile")
             .prefetch_related("tags")
+            .annotate(answers_count=Count("answers"))
         )
 
     def get_hot_questions(self) -> QuerySet:
@@ -27,6 +28,7 @@ class QuestionRepository(IQuestionRepository):
             Question.objects.hot()
             .select_related("author", "author__profile")
             .prefetch_related("tags")
+            .annotate(answers_count=Count("answers"))
         )
 
     def get_questions_by_tag(self, tag_name: str) -> QuerySet:
@@ -35,6 +37,7 @@ class QuestionRepository(IQuestionRepository):
             Question.objects.by_tag(tag_name)
             .select_related("author", "author__profile")
             .prefetch_related("tags")
+            .annotate(answers_count=Count("answers"))
         )
 
     def get_question_by_id(self, question_id: int) -> Question | None:
